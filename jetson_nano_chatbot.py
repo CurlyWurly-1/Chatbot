6b82aaaf0837e3f23c04f05ca85bae48a89075e4
CurlyWurly-1 <CurlyWurly-1@users.noreply.github.com>
2022-09-17 11:38:29 +0100

    Update jetson_nano_chatbot.py

--- jetson_nano_chatbot.py
+++ jetson_nano_chatbot.py
@@ -1,11 +1,30 @@
 # Python program to translate
 # speech to text and text to speech
  
- 
 import speech_recognition as sr
 from subprocess import Popen
 import os
 import openai
+botName      = "Buddy"
+myName       = "Pete"
+myAge        = "202"
+myBirthDate  = "1st April 1801" 
+myBirthPlace = "Cardiff"
+myLocation   = "Ruabon"
+
+def InfoTextSet (botName, myName, myAge, myBirthDate, myBirthPlace, myLocation):
+    infoText = ""
+    infoText = botName + " is a chatbot that cheerfully answers questions\n"
+    infoText + infoText + "You:What is your name?\n" +botName +":My name is " + botName + "\n" 
+    infoText = infoText + "You:What is my name?\n" + botName +":Your name is " + myName + "\n" 
+    infoText = infoText + "You:How old am I?\n" + botName +":You are " + myAge + " years old\n" 
+    infoText = infoText + "You:When was I born?\n" + botName + ":You were born on " + myBirthDate +"\n" 
+    infoText = infoText + "You:Where was I born?\n" + botName +":You were born in "+ myBirthPlace + "\n" 
+    infoText = infoText + "You:Where am I now?\n" + botName +":You are in "+ myLocation + "\n" 
+    infoText = infoText + "You:What is my location now?\n" + botName +"Your location is "+ myLocation + "\n" 
+    infoText = infoText + "You:Where do I live now?\n" + botName +"You live in "+ myLocation + "\n" 
+    return infoText 
+
 
 def GPT_Completion(texts):
 ## Call the API key under your account (in a secure way)
@@ -23,7 +42,6 @@ def GPT_Completion(texts):
  
 # Initialize the recognizer
 r = sr.Recognizer()
-speech = sr.Microphone(device_index=11)
 
 
 # Find out which device you want to use, and amend lines 26 and 51 with the device number (starting from zero) 
@@ -40,18 +58,31 @@ speech = sr.Microphone(device_index=11)
 #'tegra-snd-t210ref-mobile-rt565x: - (hw:1,9)', 
 #'CD002: USB Audio (hw:2,0)', 
 #'Canyon CNS CWC5 Webcam: USB Audio (hw:3,0)', 'hdmi', 'pulse', 'music', 'default']
+
+
+print("Speaking my introduction")   
+gptText = "Hi " + myName + ", I am " + botName + ", Please talk to me or ask a question whenever you see the listening prompt"   
+p1 = Popen(['python3', 'speak.py', gptText,])    # For Jetson Nano
+p1_status = p1.wait()
+
      
-     
-# Loop infinitely for user to
-# speak
+# Mic List
 mic_list = sr.Microphone.list_microphone_names()
 print(mic_list)
 
-# use the microphone as source for input.
-with sr.Microphone(device_index=11) as source:
-    #listens for the user's input
-    print("Adusting for background noise")
-    audio = r.adjust_for_ambient_noise(source) 
+try:
+    device_no = int(12)
+    with sr.Microphone(device_index=device_no) as source:
+        print("Please be quiet - Adjusting for background noise")
+        #listens for the user's input
+        r.adjust_for_ambient_noise(source)
+except:
+    print("Device 12 didn't work, trying device 11")
+    device_no = int(11)
+    with sr.Microphone(device_index=device_no) as source:
+        print("Please be quiet - Adjusting for background noise")
+        #listens for the user's input
+        r.adjust_for_ambient_noise(source)    
 
 
 while(1):   
@@ -61,19 +92,52 @@ while(1):
     try:
          
         # use the microphone as source for input.
-        with sr.Microphone(device_index=11) as source:
+        with sr.Microphone(device_index=device_no) as source:
             #listens for the user's input
+#            print("Adjusting")
+            r.adjust_for_ambient_noise(source, duration =0.2)    
             print("Listening")
-#            audio = r.adjust_for_ambient_noise(source)    
-            audio = r.listen(source)
+            audio = r.listen(source, phrase_time_limit=8)
             print("processing")             
 
         try:    
-            MyText = r.recognize_google(audio, language = 'en-UK')    
-            print("Q: "+MyText)            
-            GptText = GPT_Completion(MyText)
-            print(GptText)   
-            p1 = Popen(['python3', 'speak.py', GptText,])    # For Jetson Nano
+            myText = r.recognize_google(audio, language = 'en-UK')    
+            print("Q: "+myText)            
+            myText = InfoTextSet(botName, myName, myAge, myBirthDate, myBirthPlace, myLocation) + myText
+            gptText = GPT_Completion(myText)
+            print(gptText)   
+
+            try:
+                if (gptText[0] == '?') :
+                    gptText = gptText[1:]
+                if ('?' in gptText) :
+                    temp = gptText
+                    zexit = False
+                    while zexit == False :
+                        if (temp[0] == '?') :
+                            zexit = True
+                        temp = temp[1:]
+                    if temp != "":
+                        gptText = temp
+                if ('You:' in gptText) :
+                    zexit = False
+                    while zexit == False :
+                        if (gptText[0] == ':') :
+                            zexit = True
+                        gptText = gptText[1:]
+                if (':' in gptText) :
+                    zexit = False
+                    while zexit == False :
+                        if (gptText[0] == ':') :
+                            zexit = True
+                        gptText = gptText[1:]
+                if ('http' in gptText) :
+                    gptText = "Please refine your question"
+            except:
+                if gptText != "": 
+                    print("error - index out of range")    
+
+            p1 = Popen(['python3', 'speak.py', gptText,])    # For Jetson Nano
 #            (output, err) = p1.communicate()  
             #This makes the wait possible
             p1_status = p1.wait()
@@ -90,3 +154,4 @@ while(1):
          
     except sr.UnknownValueError:
         print("Sounds ignored")
+
