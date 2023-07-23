d609508dee6b1f39ccd231c5ab7fcde3f5829b43
CurlyWurly-1 <CurlyWurly-1@users.noreply.github.com>
2023-07-23 10:17:55 +0100

    Update speak.py

--- speak.py
+++ speak.py
@@ -33,7 +33,8 @@ def say_hi_internet(name):
     except:
         pass
     myobj.save("welcome1.mp3")
-    playsound("welcome1.mp3")
+    os.system("play welcome1.mp3 tempo 1.3")   # If this doesn't work, comment out and use the playsound command as below
+#    playsound("welcome1.mp3")
     os.remove("welcome1.mp3")
 
 
